20d74529fc18a9230e58af52ac2ffeba2d8c06de
jasonglenevans <jasonglenevans2010@gmail.com>
2022-10-09 11:02:57 -0400

    upload

--- upload.sh
+++ upload.sh
@@ -4,8 +4,5 @@ git commit -m "upload"
 git rm -r --cached "node_modules"
 git commit -m 'Remove the now ignored directory node_modules'
 git branch -M main
-git rm -r --cached "dist"
-git commit -m 'Remove the now ignored directory node_modules'
-git branch -M main
 git remote add origin https://github.com/jasonglenevans/gvbvdxx-midi.git
 git push -f origin main
